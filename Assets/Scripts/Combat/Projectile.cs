bf145ce261805001216c662c0afe7426ad0514fe
Zachary Dowd <zacharyd2796@zohomail.com>
2022-07-15 11:30:47 -0600

    added projectile

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -1,17 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField] Transform target = null;
     [SerializeField] float projectileSpeed = 5f;
 
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
-        transform.Translate(0, 0, projectileSpeed * Time.deltaTime);
+        if (target == null) return;
+
+        transform.LookAt(GetAimLocation());
+        transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+        if (targetCapsule == null)
+        {
+            return target.position;
+        }
+        return target.position + Vector3.up * targetCapsule.height / 1.5f;
     }
 }
